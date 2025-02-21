0f8127f0e4c39c5013db971b30b40c56cdeaaf60
gigamicro <gammamicro@gmail.com>
2025-02-21 16:23:13 -0500

    jarzip

--- watchcopy.sh
+++ watchcopy.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 cd "$(readlink -zf "$0" | xargs -0 dirname)"
-rm technetium-origins.zip
+rm technetium-origins.jar
 while true; do
-	zip -ru technetium-origins.zip assets data pack.mcmeta
-	cp -uvft ../profile/saves/New\ World_"$(git branch --show-current)"/datapacks/ ./*.zip
-	cp -uvft ../profile/resourcepacks ./*.zip
+	zip -ru technetium-origins.jar -- assets data pack.mcmeta;
+	cp -uvft ../profile/mods ./*.jar
 	sleep 0.25 & tail --pid $! -n0 -f ../profile/logs/latest.log
 done
