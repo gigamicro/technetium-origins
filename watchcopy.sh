be2ff69206b8b35a7a32869c24d0f51c937b2a76
gigamicro <gammamicro@gmail.com>
2025-02-21 16:33:14 -0500

    fabric

--- watchcopy.sh
+++ watchcopy.sh
@@ -2,7 +2,7 @@
 cd "$(readlink -zf "$0" | xargs -0 dirname)"
 rm technetium-origins.jar
 while true; do
-	zip -ru technetium-origins.jar -- assets data pack.mcmeta;
+	zip -ru technetium-origins.jar -- assets data pack.mcmeta fabric.mod.json
 	cp -uvft ../profile/mods ./*.jar
 	sleep 0.25 & tail --pid $! -n0 -f ../profile/logs/latest.log
 done
